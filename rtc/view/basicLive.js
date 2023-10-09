01dab8b600f14c0139864082c7b2cb99fd3dd143
oikomi <miaohong@miaohong.org>
2023-10-09 20:29:30 +0800

    init

--- rtc/view/basicLive.js
+++ rtc/view/basicLive.js
@@ -148,7 +148,7 @@ async function send_cmd() {
     send_user: options.uid
   })
   .then(function (response) {
-    alert('send cmd!', 'success')
+    alert('send cmd success!')
     console.log(response);
   })
   .catch(function (error) {
