9cc451bc9c79ed9e65b9a28c0fdc8b3e159c03fb
oikomi <miaohong@miaohong.org>
2023-10-21 23:16:29 +0800

    init

--- rtc2/fatbear-web/src/config/config.ts
+++ rtc2/fatbear-web/src/config/config.ts
@@ -4,5 +4,5 @@ export const APP_ID = "ac1f26c994ea4f978a11ce1251424920"
 export const TOKEN = "007eJxTYDjPtvje1BCGGDUrM9GTu3Osu398XXx+7p1Z/9dk+VculOxQYEhMNkwzMku2tDRJTTRJszS3SDQ0TE41NDI1NDEysTQymPzSOLUhkJHBQyGAhZEBAkF8doa0xJKk1MQiBgYA1xYgfw=="
 
 
-export const SERVER_BASE = "http://127.0.0.1:8080/"
-// export const SERVER_BASE = "https://120.55.60.98/"
+// export const SERVER_BASE = "http://127.0.0.1:8080/"
+export const SERVER_BASE = "https://120.55.60.98/"
