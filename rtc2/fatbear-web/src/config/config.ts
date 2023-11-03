790ddd2c2c7f7f89f4ab1fd2072e97dd7d0e63c1
oikomi <miaohong@miaohong.org>
2023-11-03 22:13:51 +0800

    init

--- rtc2/fatbear-web/src/config/config.ts
+++ rtc2/fatbear-web/src/config/config.ts
@@ -7,4 +7,4 @@ export const TOKEN = "007eJxTYGhf6Nwaqxp74/q6ieriM3QOXT6i4HGkh3GN+OPHL9c7V5UpMCQ
 // export const SERVER_BASE = "http://127.0.0.1:8080/"
 export const SERVER_BASE = "https://120.55.60.98/"
 
-export const MSG_INTERVAL = 4000000
+export const MSG_INTERVAL = 4000
